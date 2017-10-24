1cb762ee0657ef178a523c311ffa5ca58088f4b6
Jimmy Fish <killcoder212@gmail.com>
2017-10-25 05:16:30 +0700

    add function to right menu

--- public/js/global.js
+++ public/js/global.js
@@ -1,3 +1,21 @@
 $(function() {
-    
+    $('[data-toggle="popover"]').popover({
+        html: true,
+        title: function() {
+            return $(this).next().find('.popover-head').html();
+        },
+        content: function() {
+            return $(this).next().find('.popover-content').html();
+        }
+    });
+    $('[id^="item"]').on('shown.bs.popover', function() {
+        var itemPos = parseInt($(this).offset().top);
+
+        var absPos = itemPos - 70;
+        $('.popover.left .arrow').css('top', absPos + 'px');
+    });
+
+    $('[data-toggle="popover"]').on('click', function(e) {
+        $('[data-toggle="popover"]').not(this).popover('hide');
+    });
 });
