799457ceb38dac8a79f7ec77e0c8e9ae931a915b
Jimmy Fish <killcoder212@gmail.com>
2017-10-28 10:03:44 +0700

    styling form

--- public/js/global.js
+++ public/js/global.js
@@ -1,23 +1,43 @@
 $(function() {
-    $('[data-toggle="popover"]').popover({
-        html: true,
-        title: function() {
-            return $(this).next().find('.popover-head').html();
-        },
-        content: function() {
-            return $(this).next().find('.popover-content').html();
+
+    var showPopover = $.fn.popover.Constructor.prototype.show;
+    $.fn.popover.Constructor.prototype.show = function() {
+        showPopover.call(this);
+        if (this.options.showCallback) {
+            this.options.showCallback.call(this);
         }
-    });
-    $('[id^="item"]').on('shown.bs.popover', function() {
-        var itemPos = parseInt($(this).offset().top);
+    }
 
-        var absPos = itemPos - 70;
-        $('.popover.left .arrow').css('top', absPos + 'px');
-    });
+    var itemData = $('[id^="item"]');
 
-    $('[data-toggle="popover"]').on('click', function(e) {
-        $('[data-toggle="popover"]').not(this).popover('hide');
-    });
+    itemData.popover({
+            html: true,
+            title: function() {
+                return $(this).next().find('.popover-head').html();
+            },
+            content: function() {
+                return $(this).next().find('.popover-content').html();
+            },
+            showCallback: function() {
+
+                $(".popover-content select").select2({
+                    containerCss: {
+                        "display": "block"
+                    },
+                    minimumResultsForSearch: -1
+                });
+            },
+        })
+        .on('shown.bs.popover', function() {
+            var itemPos = parseInt($(this).offset().top);
+
+            var absPos = itemPos - 70;
+            $('.popover.left .arrow').css('top', absPos + 'px');
+        })
+        .on('click', function(e) {
+            e.preventDefault();
+            $('[data-toggle="popover"]').not(this).popover('hide');
+        });
 
     $('.slider-inner').slick({
         infinite: true,
